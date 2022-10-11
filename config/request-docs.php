8be8f4388dd55401df52c7ff8b5cb3d244b2d3ac
Fábio Galvão <fgalvao90@gmail.com>
2022-10-11 15:47:33 +0200

    Change default route sorting

--- config/request-docs.php
+++ config/request-docs.php
@@ -23,7 +23,7 @@ return [
     /**
      * Sorting route by and there is two types default(route methods), route_names.
      */
-    'sort_by' => 'default',
+    'sort_by' => 'route_names',
 
     //Use only routes where ->uri start with next string Using Str::startWith( . e.g. - /api/mobile
     'only_route_uri_start_with' => 'api',
